9ecb376ee8e985ac029a343643be1ec6b1d660a2
mlaakso@dev <laakso.mavrick@gmail.com>
2018-01-03 23:26:30 -0500

    awful hover

--- client/src/components/BrandBar.vue
+++ client/src/components/BrandBar.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="brand-wrapper">
-    <span>{{ username }}</span>
+<div class="brand-menu-wrapper">
+    <div 
+      v-on:mouseover="dropdown = true"
+      v-on:mouseleave="dropdown = false"
+      class="brand-menu">
+      <span>Signed in as {{ username }}</span>
+      <div v-bind:class="{ visible: dropdown }" class="brand-menu-dropdown-container">
+        <div class="brand-menu-dropdown">
+          <a class="dropdown-item">Logout</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +21,12 @@ import { mapGetters } from 'vuex'
 export default {
 
   name: 'BrandBar',
+
+  data: function() {
+    return {
+      dropdown: false
+    }
+  },
 
   mounted: function() {
     console.log(this.get_user)
@@ -38,14 +54,47 @@ export default {
   flex-direction: row;
 }
 
-.brand-wrapper {
+.brand-menu-wrapper {
   margin-left: auto;
   margin-top: auto;
   margin-bottom: auto;
   margin-right: 2em;
 }
 
-.brand-wrapper span {
+.brand-menu-wrapper span {
   color: white;
+}
+
+.brand-menu-dropdown-container {
+  padding-top: 4px;
+  position: absolute;
+  right: 32px;
+  opacity: 0;
+  transition: opacity 100ms;
+  z-index: -1;
+}
+
+.brand-menu-dropdown-container:hover {
+  opacity: 1;
+  display: inherit;
+}
+
+.brand-menu-dropdown {
+  background: #fff;
+  padding: 8px 16px;
+  border-radius: 5px;
+}
+
+.dropdown-item {
+  display: block;
+  padding: 4px 0;
+  /*border-bottom: 1px solid #eaeaea;*/
+  cursor: pointer;
+  min-width: 100px;
+}
+
+.visible {
+  z-index: 1;
+  opacity: 1; 
 }
 </style>
